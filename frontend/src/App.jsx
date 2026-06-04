79338a3ebc74b8e25fa69a7f2e78afb62b18dc28
Ryan Taylor <rtaylor003376@gmail.com>
2026-06-04 16:08:48 -0400

    I added the home page and some components as well as added some routes to the recipe route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import RecipeDetail from "./pages/RecipeDetail";
 import Admin from "./pages/Admin";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/recipe/:id" element={<RecipeDetail />} />
-      <Route path="*" element={<RecipeDetail />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="*" element={<RecipeDetail />} />
     </Routes>
   );
 }
